c9913444765f13e555dcf4ff536ff58a10ff4d72
dhritimanbera88-ECE068 <dhritimanbera88@gmail.com>
2026-04-15 23:34:38 +0530

    Add palindrome checker using array in C

---
+++ palindrome number using array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int num, a[10], i = 0, j;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        a[i] = num % 10;
+        num = num / 10;
+        i++;
+    }
+
+    int flag = 1;
+
+    for (j = 0; j < i / 2; j++) {
+        if (a[j] != a[i - j - 1]) {
+            flag = 0;
+            break;
+        }
+    }
+
+    if (flag == 1)
+        printf("Number is Palindrome");
+    else
+        printf("Number is not Palindrome");
+
+    return 0;
+}
